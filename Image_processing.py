210f9064262d3131860b2272a61ba3bd3679ad77
Mateusz Kapiszewski <kapiszewski.mateusz@gmail.com>
2017-10-26 16:07:43 +0200

    Images on subplot

--- Image_processing.py
+++ Image_processing.py
@@ -1,6 +1,34 @@
+from os import listdir
+import os
+from os.path import isfile, join
 from skimage import data, io
 from matplotlib import pyplot as plt
+import re
 
-image = data.coins() # Albo: coins(), page(), moon()
-io.imshow(image)
-plt.show()
+
+def findNamesOfPictures():
+    mypath = os.path.dirname(os.path.realpath(__file__))
+    onlyFiles= [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    nameOfImagesToProcessing=[]
+    for fileName in onlyFiles:
+
+            if re.match("^samolot...jpg$", fileName):
+                nameOfImagesToProcessing.append(fileName)
+
+    return nameOfImagesToProcessing
+
+
+def showImages():
+    fig, axes = plt.subplots(5, 5, figsize=(10,10), sharex= True, sharey= True)
+    ax = axes.ravel();
+    for i in range (len(imageCollection)):
+        ax[i].imshow(imageCollection[i], aspect="auto", interpolation="nearest")
+        ax[i].set_title(imageNames[i])
+        ax[i].axis('off')
+    plt.savefig('test.pdf')
+
+
+if __name__ == "__main__":
+    imageNames = findNamesOfPictures()
+    imageCollection = io.ImageCollection(imageNames)
+    showImages()
