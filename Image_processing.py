7fd8466dc65f5d4b2563614e081b31c87e205a35
Mateusz Kapiszewski <kapiszewski.mateusz@gmail.com>
2017-10-30 23:55:38 +0100

    Some contours are visible

--- Image_processing.py
+++ Image_processing.py
@@ -1,9 +1,13 @@
-from os import listdir
 import os
-from os.path import isfile, join
-from skimage import data, io
-from matplotlib import pyplot as plt
 import re
+from os import listdir
+from os.path import isfile, join
+
+import numpy as np
+from matplotlib import pyplot as plt
+from skimage import data, measure, filters
+from skimage.exposure import exposure
+from skimage.filters import scharr
 
 
 def findNamesOfPictures():
@@ -18,17 +22,47 @@ def findNamesOfPictures():
     return nameOfImagesToProcessing
 
 
-def showImages():
+def showImages(images, contours):
+    print("wypisuje zdjecia")
     fig, axes = plt.subplots(5, 5, figsize=(10,10), sharex= True, sharey= True)
     ax = axes.ravel();
-    for i in range (len(imageCollection)):
-        ax[i].imshow(imageCollection[i], aspect="auto", interpolation="nearest")
+    for i in range(len(images)):
+        ax[i].imshow(images[i], interpolation="nearest", extent=[-2, 4, -2, 4], cmap="gray")
         ax[i].set_title(imageNames[i])
         ax[i].axis('off')
+        for n, contour in enumerate(contours[i]):
+            if n > 10:
+                break
+            temp = contour
+            ax[i].plot(temp[:, 1], temp[:, 0], linewidth=1)
+
     plt.savefig('test.pdf')
+
+
+def findContours(images):
+    result = []
+    for i in range(len(images)):
+        p2, p98 = np.percentile(images[i], (0, 95))
+        images[i] = exposure.rescale_intensity(images[i], in_range=(p2, p98))
+
+        images[i] = scharr(images[i])
+        images[i] = filters.sobel(images[i])
+        result.append(measure.find_contours(images[i], level=0.14, fully_connected="high", positive_orientation="high"))
+
+    # result= measure.find_contours(images[0], level=0.7)
+    return result
+
+
+def loadImages(names):
+    result = []
+    for i in range(len(names)):
+        result.append(data.imread(names[i], as_grey=True))
+    return result
+
 
 
 if __name__ == "__main__":
     imageNames = findNamesOfPictures()
-    imageCollection = io.ImageCollection(imageNames)
-    showImages()
+    imageCollection = loadImages(imageNames)
+    contours = findContours(imageCollection)
+    showImages(imageCollection, contours)
