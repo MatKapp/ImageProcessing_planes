5ff0fd54d03c3c2b11b5cc179dd4110a82913290
Mateusz Kapiszewski <kapiszewski.mateusz@gmail.com>
2017-11-04 20:44:47 +0100

    shows contours but doesn't find them

--- Image_processing.py
+++ Image_processing.py
@@ -20,6 +20,29 @@ def findNamesOfPictures():
     return nameOfImagesToProcessing
 
 
+def findContours(images):
+    result = []
+    goodContours = []
+    for i in range (len (images)):
+        # ret, thresh = cv2.threshold (images[i], 127, 255, 0)
+        image, contours, hierarchy = cv2.findContours (np.array (images[i]), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        for j in range (len (contours)):
+            print (cv2.contourArea (contours[j]))
+            if (cv2.contourArea (contours[j]) > 100000):
+                goodContours.append (contours[j])
+        result.append (goodContours)
+    return result
+
+
+def drawContours(images, contours):
+    result = []
+    for i in range (len (images)):
+        print (type (np.array (contours[i])))
+        print ((np.array (contours[i])).shape)
+        result.append (cv2.drawContours (np.array (images[i]), np.array (contours[i]), -1, (155, 155, 155), 1))
+    return result
+
+
 def showImages(images):
     print("wypisuje zdjecia")
     fig, axes = plt.subplots(5, 5, figsize=(10,10), sharex= True, sharey= True)
@@ -35,13 +58,6 @@ def showImages(images):
         #     ax[i].plot(temp[:, 1], temp[:, 0], linewidth=1)
 
     plt.savefig('test.pdf')
-
-
-def findContours(images):
-    result = []
-
-    # result= measure.find_contours(images[0], level=0.7)
-    return result
 
 
 def loadImages(names):
@@ -64,7 +80,9 @@ def denoisingImages(images):
     result = []
     for i in range(len(images)):
         temp = images[i]
+        kernel = np.ones ((5, 5), np.uint8)
         temp = cv2.fastNlMeansDenoising(temp, h=10, templateWindowSize=7, searchWindowSize=21)
+        temp = cv2.morphologyEx (temp, cv2.MORPH_OPEN, kernel)
         result.append(temp)
     return result
 
@@ -73,16 +91,74 @@ def filteringImages(images):
     result = []
     for i in range(len(images)):
         temp = images[i]
-        temp = cv2.Canny(temp, 100, 250, L2gradient=True)
+
+        # hist, bins = np.histogram (temp, 256, [0, 256])
+        # cdf = hist.cumsum ()
+        # cdf_normalized = cdf * hist.max () / cdf.max ()
+        # cdf_m = np.ma.masked_equal (cdf, 0)
+        # cdf_m = (cdf_m - cdf_m.min ()) * 255 / (cdf_m.max () - cdf_m.min ())
+        # cdf = np.ma.filled (cdf_m, 0).astype ('uint8')
+        # temp=cdf[temp]
+
+
+
+        blur = cv2.GaussianBlur (temp, (5, 5), 0)
+        ret2, temp = cv2.threshold (blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        # temp = cv2.adaptiveThreshold (temp, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        #                             cv2.THRESH_BINARY, 11, 2)
+        #temp = cv2.Canny(temp, 50, 550, L2gradient=True)
+
+
         result.append(temp)
     return result
+
+
+def gamma_correction(img, correction):
+    img = img / 255.0
+    img = cv2.pow (img, correction)
+    return np.uint8 (img * 255)
+
+
+def filteringImages2(images):
+    result = []
+    for i in range (len (images)):
+        temp = images[i]
+
+        gamma = 0.5
+        kernel = np.ones ((5, 5), np.uint8)
+
+        temp = cv2.medianBlur (temp, 5)
+        temp = gamma_correction (temp, gamma)
+        temp = cv2.Laplacian (temp, cv2.CV_8UC1)
+        temp = cv2.dilate (temp, kernel, iterations=5)
+        temp = cv2.erode (temp, kernel, iterations=2)
+        # temp=cv2.bitwise_not(temp)
+        # hist, bins = np.histogram (temp, 256, [0, 256])
+        # cdf = hist.cumsum ()
+        # cdf_normalized = cdf * hist.max () / cdf.max ()
+        # cdf_m = np.ma.masked_equal (cdf, 0)
+        # cdf_m = (cdf_m - cdf_m.min ()) * 255 / (cdf_m.max () - cdf_m.min ())
+        # cdf = np.ma.filled (cdf_m, 0).astype ('uint8')
+        # temp=cdf[temp]
+
+        # blur = cv2.GaussianBlur (temp, (5, 5), 0)
+        # ret2, temp = cv2.threshold (blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        # temp= cv2.Canny(temp, 100, 200)
+
+        # temp= morphology.binary_fill_holes(temp)
+        result.append (temp)
+    return result
+
 
 if __name__ == "__main__":
     imageNames = findNamesOfPictures()
     imageCollection = loadImages(imageNames)
     imageCollection = np.array(imageCollection)
-    imageCollection = denoisingImages(imageCollection)
-    imageCollection = filteringImages(imageCollection)
-    # imageCollection = rgbBgrConversion(imageCollection)
-    # contours = findContours(imageCollection)
+    imageCollection = denoisingImages (imageCollection)
+    imageCollection = filteringImages2 (imageCollection)
+
+    # contours = findContours (imageCollection)
+    # imageCollection = drawContours(imageCollection, contours)
+
+
     showImages(imageCollection)
